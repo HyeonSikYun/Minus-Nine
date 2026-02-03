c96825f64d47d4c1ee3c9465e3ec0a45d33f38fb
HyeonSikYun <109672921+HyeonSikYun@users.noreply.github.com>
2026-02-04 04:47:55 +0900

    오디오 매니저 작업중

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -2,8 +2,42 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance { get; private set; }
+
+    [Header("Audio Source")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
+    [Header("Audio Clip")]
+    public AudioClip mainBgm;
+    public AudioClip tutorialBgm;
+    public AudioClip btnClick;
+    public AudioClip gunPickup;
+    public AudioClip Rifle;
+    public AudioClip Bazooka;
+    public AudioClip flameThrower;
+    public AudioClip explosion;
+    public AudioClip reload;
+    public AudioClip zombieChase;
+    public AudioClip generateOn;
+    public AudioClip elevatorAmbience;
+    public AudioClip elevatorDing;
+    public AudioClip footStep;
+    public AudioClip gunHit;
 
+    private void Awake()
+    {
+        if (Instance == null) { Instance = this; DontDestroyOnLoad(gameObject); }
+        else { Destroy(gameObject); return; }
+    }
+    private void Start()
+    {
+        musicSource.clip=tutorialBgm;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
 }
